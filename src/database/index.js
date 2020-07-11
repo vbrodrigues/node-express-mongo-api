094fe926ef61693a4a8bc9e205a4743cbcdf72e9
unknown <vitorborbarodrigues@gmail.com>
2020-07-11 16:02:02 -0300

    removed toy password

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://vitor:221193Vbr@node-api-test.ravlj.gcp.mongodb.net/node-api-test?retryWrites=true&w=majority', { 
+// Replace <password> and <dbname> with user password and database name from MongoDB
+mongoose.connect('mongodb+srv://vitor:<password>@node-api-test.ravlj.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority', { 
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
